64e4c4911759ba538b58d64cfeb325fbc6f3250a
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2020-10-27 00:10:15 +0200

    Create a new file that contains the content of the words & numbers merged side by side

--- 1_1_2.sh
+++ 1_1_2.sh
@@ -11,3 +11,9 @@ mkdir Lab1
 
 # Copy the files “words” & “numbers” 
 cp words numbers Lab1
+
+# Create a new file that contains the content of the words & numbers merged side by side and save it to a
+# file called “MergedContent”.
+# the -d', ' argument specifies to use a comma and space as a delimiter between the contents of each file
+cd Lab1
+paste -d', ' words numbers >> MergedContent
